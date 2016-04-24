469a5e937e73de1658f5fd6e0637c19bd5b86038
Florian Feldhaus <florian.feldhaus@gmail.com>
2016-04-25 00:48:01 +0200

    Updated OCI Pester Tests
    The Tests are still work in progress as REST API calls are hard to mock

--- src/OnCommand-Insight.Tests.ps1
+++ src/OnCommand-Insight.Tests.ps1
@@ -1,6 +1,51 @@
-﻿$ScriptPath = Split-Path -Parent $PSCommandPath
+﻿Import-Module "$PSScriptRoot\OnCommand-Insight"
 
-Import-Module $ScriptPath\OnCommand-Insight.psm1
+$Globa:OciServerName = 'ff-oc1.muccbc.hq.netapp.com'
+$Global:OciCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "admin",("admin123" | ConvertTo-SecureString -AsPlainText -Force)
+
+$global:OciTestServer = Connect-OciServer -Name $Name -Credential $Credential -Insecure
+
+Describe "Credential Management" {
+    BeforeEach {
+        $Credential = $null
+        $OciCredential = $null
+        $HostName = $null
+        $Password = $null
+
+        Remove-OciCredential -Name example.com
+    }
+    
+    AfterEach {
+        $Credential = $null
+        $OciCredential = $null
+        $HostName = $null
+        $Password = $null
+
+        Remove-OciCredential -Name example.com
+    }
+
+    Context "adding, retrieving and removing of Credentials" {
+        it "successfully adds, retrieves and removes a credential with hostname example.com and valid credential" {
+            $HostName = "example.com"
+            $UserName = "user"
+            $Password = "password"
+            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $UserName,($Password  | ConvertTo-SecureString -AsPlainText -Force)
+
+            Add-OciCredential -Name $HostName -Credential $Credential
+
+            $OciCredential = Get-OciCredential -Name $HostName
+
+            $OciCredential.Credential.UserName | Should Be $UserName
+            $OciCredential.Credential.GetNetworkCredential().Password | Should Be $Password
+
+            Remove-OciCredential -Name $HostName
+
+            $OciCredential = Get-OciCredential -Name $HostName
+
+            $OciCredential | Should Be $null
+        }
+    }
+}
 
 Describe "Connect-OciServer" {
     BeforeEach {
@@ -14,48 +59,43 @@ Describe "Connect-OciServer" {
     }
 
     Context "initiates a connection to an OnCommand Insight Server" {
-        Mock -ModuleName OnCommand-Insight Invoke-RestMethod { 
-            if ($Record) {
-                Write-Host $Uri
-                Write-Host $Headers
-                $Result = Microsoft.PowerShell.Utility\Invoke-RestMethod -Body $Body `
-                                            -Headers $Headers `
-                                            -Method $Method `
-                                            -OutFile $ScriptPath\$Version-recording.json `
-                                            -Uri $Uri
-            }
-            else {
-                $FileName = "$ScriptPath\$Version-recording.json"
-                if (Test-Path -Path $FileName) {
-                    $Result = Get-Content $FileName
-                }
-                else {
-                    throw "Recording $FileName does not exist, run first with -Recording, -Server and -Version parameters"
-                }
-            }
-
-            $Result
-        }
 
         it "succeeds with parameters Name, Credential and Insecure" {
-            $OciServer = Connect-OciServer -Name $Server -Credential $Credential -Insecure
-            $OciServer.Name | Should Be $Server
-            #$Server.APIVersion | Should Be $MockProxyAPIVersion
+            $OciServer = Connect-OciServer -Name $OciServerName -Credential $OciCredential -Insecure
+            $OciServer.Name | Should Be $OciServerName
             $Global:CurrentOciServer | Should Be $OciServer
         }
     }
-    #Context "initiates a connection to an OnCommand Insight Server without setting the global variable `$Global:CurrentOciServer" {
-    #    Mock -ModuleName OnCommand-Insight Get-OciProxy { return $MockProxy }
-    #    Mock -ModuleName OnCommand-Insight New-WebServiceProxy { return $MockProxy }
-    #    it "succeeds with parameters Name, Credential and Transient" {
-    #        $Global:CurrentOciServer = $null
-    #        $Server = Connect-OciServer -Name example.com -Credential $Credential -Transient
-    #        $Server.Name | Should Be "example.com"
-    #        $Server.SANScreenVersion | Should Be $MockProxySANScreenVersion
-    #        $Server.Limit | Should Be 1000
-    #        $Server.APIVersion | Should Be $MockProxyAPIVersion
-    #        $Global:CurrentOciServer | Should BeNullOrEmpty
-    #        Assert-MockCalled New-WebServiceProxy -ModuleName OnCommand-Insight -Times 1
-    #    }
-    #}
+    
+    Context "initiates a connection to an OnCommand Insight Server without setting the global variable `$Global:CurrentOciServer" {
+        it "succeeds with parameters Name, Credential and Transient" {
+            $Server = Connect-OciServer -Name example.com -Credential $Credential -Transient
+            $Server.Name | Should Be $ServerName
+            $Global:CurrentOciServer | Should BeNullOrEmpty
+        }
+    }
+}
+
+Describe "Get-OciAcquisitionUnits" {
+    BeforeEach {
+        $OciServer = Connect-OciServer -Name $OciServerName -Credential $OciCredential -Insecure
+        $Global:CurrentOciServer = $null
+        $AcquisitionUnits = $null
+    }
+
+    AfterEach {
+        $OciServer = $null
+        $Global:CurrentOciServer = $null
+    }
+
+    Context "gets Acquisition Units" {
+        $AcquisitionUnits = Get-OciAcquisitionUnits
+        $AcquisitionUnits | Should Not BeNullOrEmpty
+    }
+
+    Context "gets Acquisition Units with datasources" {
+        $AcquisitionUnits = Get-OciAcquisitionUnits -datasources
+        $AcquisitionUnits | Should Not BeNullOrEmpty
+        $AcquisitionUnits.datasources | Should Not BeNullOrEmpty
+    }
 }
