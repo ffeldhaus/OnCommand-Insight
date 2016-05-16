61b4d3fff3869ab8a5938e9b93d64e49a5b72665
Florian Feldhaus <florian.feldhaus@gmail.com>
2016-05-16 14:24:42 +0200

    restructuring of cmdlets

--- src/OnCommand-Insight.psm1
+++ src/OnCommand-Insight.psm1
@@ -339,153 +339,6 @@ function ParseExceptionBody($Response) {
     }
 }
 
-<#
-.EXAMPLE
-Connect-OciServer -Name ociserver.example.com -Credential (Get-Credential)
-
-Name       : ociserver.example.com
-BaseURI    : https://ociserver.example.com
-Credential : System.Management.Automation.PSCredential
-Headers    : {Authorization}
-APIVersion : 1.2
-Timezone   : System.CurrentSystemTimeZone
-#>
-function global:Connect-OciServer {
-    [CmdletBinding()]
- 
-    PARAM (
-        [parameter(Mandatory=$True,
-                   Position=0,
-                   HelpMessage="The name of the OCI Server. This value may also be a string representation of an IP address. If not an address, the name must be resolvable to an address.")][String]$Name,
-        [parameter(Mandatory=$False,
-                   Position=1,
-                   HelpMessage="A System.Management.Automation.PSCredential object containing the credentials needed to log into the OCI server.")][System.Management.Automation.PSCredential]$Credential,
-        [parameter(Mandatory=$False,
-                   Position=2,
-                   HelpMessage="This cmdlet always tries to establish a secure HTTPS connection to the OCI server, but it will fall back to HTTP if necessary. Specify -HTTP to skip the HTTPS connection attempt and only try HTTP.")][Switch]$HTTP,
-        [parameter(Mandatory=$False,
-                   Position=2,
-                   HelpMessage="This cmdlet always tries to establish a secure HTTPS connection to the OCI server, but it will fall back to HTTP if necessary. Specify -HTTPS to fail the connection attempt in that case rather than fall back to HTTP.")][Switch]$HTTPS,
-        [parameter(Mandatory=$False,
-                   Position=3,
-                   HelpMessage="If the OCI server certificate cannot be verified, the connection will fail. Specify -Insecure to ignore the validity of the OCI server certificate.")][Switch]$Insecure,
-        [parameter(Position=4,
-                   Mandatory=$False,
-                   HelpMessage="Specify -Transient to not set the global variable `$CurrentOciServer.")][Switch]$Transient,
-        [parameter(Mandatory=$False,
-                   Position=5,
-                   HelpMessage="As the timezone of the OCI Server is not available via the REST API, it needs to be manually set so that all timestamps are displayed with the correct timezone. By default the timezone will be set to the local timezone of the PowerShell environment.")][PSObject]$Timezone,
-        [parameter(Mandatory=$False,
-                   Position=6,
-                   HelpMessage="Timeout value for HTTP connections. Defaults to 600 seconds.")][Int]$Timeout
-    )
-
-    if (!$Credential) {
-        $Credential = Get-OciCredential -Name $Name | Select-Object -ExpandProperty Credential
-        if (!$Credential) {
-            throw "No Credentials supplied and $Name not in list of known OCI Servers"
-        }
-    }
- 
-    # Issue with jBoss see http://alihamdar.com/2010/06/19/expect-100-continue/
-    [System.Net.ServicePointManager]::Expect100Continue = $false
- 
-    $EncodedAuthorization = [System.Text.Encoding]::UTF8.GetBytes($Credential.UserName + ':' + $Credential.GetNetworkCredential().Password)
-    $EncodedPassword = [System.Convert]::ToBase64String($EncodedAuthorization)
-    $Headers = @{"Authorization"="Basic $($EncodedPassword)"}
- 
-    # check if untrusted SSL certificates should be ignored
-    if ($Insecure) {
-        [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-    }
-
-    # check if proxy is used
-    $ProxyRegistry = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
-    $ProxySettings = Get-ItemProperty -Path $ProxyRegistry
-    if ($ProxySettings.ProxyEnable) {
-        Write-Warning "Proxy Server $($ProxySettings.ProxyServer) configured in Internet Explorer may be used to connect to the OCI server!"
-    }
-    if ($ProxySettings.AutoConfigURL) {
-        Write-Warning "Proxy Server defined in automatic proxy configuration script $($ProxySettings.AutoConfigURL) configured in Internet Explorer may be used to connect to the OCI server!"
-    }
- 
-    if ($HTTPS -or !$HTTP) {
-        Try {
-            $BaseURI = "https://$Name"
-            $Response = Invoke-RestMethod -Method Post -Uri "$BaseURI/rest/v1/login" -TimeoutSec $Timeout -Headers $Headers
-            $APIVersion = $Response.apiVersion
-        }
-        Catch {
-            $ResponseBody = ParseExceptionBody $_.Exception.Response
-            if ($_.Exception.Message -match "Unauthorized") {
-                Write-Error "Authorization for $BaseURI/rest/v1/login with user $($Credential.UserName) failed"
-                return
-            }
-            else {
-                if ($HTTPS) {
-                    Write-Error "Login to $BaseURI/rest/v1/login failed via HTTPS protocol, but HTTPS was enforced. Exception $($_.Exception.Message)`n $ResponseBody"
-                    return
-                }
-                else {
-                    Write-Warning "Login to $BaseURI/rest/v1/login failed via HTTPS protocol. Exception message: $($_.Exception.Message)`n $ResponseBody"
-                    $HTTP = $True
-                }
-            }
-        }
-    }
-
-    if ($HTTP) {
-        Try {
-            $BaseURI = "http://$Name"
-            $Response = Invoke-RestMethod -Method Post -Uri "$BaseURI/rest/v1/login" -TimeoutSec $Timeout -Headers $Headers
-            $APIVersion = $Response.apiVersion
-        }
-        Catch {
-            $ResponseBody = ParseExceptionBody $_.Exception.Response
-            if ($_.Exception.Message -match "Unauthorized") {                
-                Write-Error "Authorization for $BaseURI/rest/v1/login with user $($Credential.UserName) failed"
-                return
-            }
-            else {
-                Write-Error "Login to $BaseURI/rest/v1/login failed via HTTP protocol. Exception message: $($_.Exception.Message)`n $ResponseBody"
-                return
-            }
-        }
-    }
-
-    if (!$Timezone) {
-        $Timezone = [timezone]::CurrentTimeZone
-    }
-    
-    if ($Timezone -isnot [Timezone]) {
-        if ([System.TimeZoneInfo]::GetSystemTimeZones().Id -contains $Timezone) {
-            $Timezone = [System.TimeZoneInfo]::GetSystemTimeZones() | ? { $_.Id -contains $Timezone }
-        }
-        else {
-            $Timezone = [timezone]::CurrentTimeZone
-        }
-    }
-
-    if (!$Timeout) {
-        $Timeout = 600
-    }
- 
-    $Server = New-Object -TypeName psobject
-    $Server | Add-Member -MemberType NoteProperty -Name Name -Value $Name
-    $Server | Add-Member -MemberType NoteProperty -Name BaseURI -Value $BaseURI
-    $Server | Add-Member -MemberType NoteProperty -Name Credential -Value $Credential
-    $Server | Add-Member -MemberType NoteProperty -Name Headers -Value $Headers
-    $Server | Add-Member -MemberType NoteProperty -Name APIVersion -Value $APIVersion
-    $Server | Add-Member -MemberType NoteProperty -Name Timezone -Value $Timezone
-    $Server | Add-Member -MemberType NoteProperty -Name Timeout -Value $Timeout
- 
-    if (!$Transient) {
-        Set-Variable -Name CurrentOciServer -Value $Server -Scope Global
-    }
- 
-    return $Server
-}
-
 function ParseAcquisitionUnits($AcquisitionUnits) {
     $AcquisitionUnits = @($AcquisitionUnits)
     foreach ($AcquisitionUnit in $AcquisitionUnits) {
@@ -669,6 +522,153 @@ function ParsePerformanceHistory($PerformanceHistory) {
         }
     }
 } 
+
+<#
+.EXAMPLE
+Connect-OciServer -Name ociserver.example.com -Credential (Get-Credential)
+
+Name       : ociserver.example.com
+BaseURI    : https://ociserver.example.com
+Credential : System.Management.Automation.PSCredential
+Headers    : {Authorization}
+APIVersion : 1.2
+Timezone   : System.CurrentSystemTimeZone
+#>
+function global:Connect-OciServer {
+    [CmdletBinding()]
+ 
+    PARAM (
+        [parameter(Mandatory=$True,
+                   Position=0,
+                   HelpMessage="The name of the OCI Server. This value may also be a string representation of an IP address. If not an address, the name must be resolvable to an address.")][String]$Name,
+        [parameter(Mandatory=$False,
+                   Position=1,
+                   HelpMessage="A System.Management.Automation.PSCredential object containing the credentials needed to log into the OCI server.")][System.Management.Automation.PSCredential]$Credential,
+        [parameter(Mandatory=$False,
+                   Position=2,
+                   HelpMessage="This cmdlet always tries to establish a secure HTTPS connection to the OCI server, but it will fall back to HTTP if necessary. Specify -HTTP to skip the HTTPS connection attempt and only try HTTP.")][Switch]$HTTP,
+        [parameter(Mandatory=$False,
+                   Position=2,
+                   HelpMessage="This cmdlet always tries to establish a secure HTTPS connection to the OCI server, but it will fall back to HTTP if necessary. Specify -HTTPS to fail the connection attempt in that case rather than fall back to HTTP.")][Switch]$HTTPS,
+        [parameter(Mandatory=$False,
+                   Position=3,
+                   HelpMessage="If the OCI server certificate cannot be verified, the connection will fail. Specify -Insecure to ignore the validity of the OCI server certificate.")][Switch]$Insecure,
+        [parameter(Position=4,
+                   Mandatory=$False,
+                   HelpMessage="Specify -Transient to not set the global variable `$CurrentOciServer.")][Switch]$Transient,
+        [parameter(Mandatory=$False,
+                   Position=5,
+                   HelpMessage="As the timezone of the OCI Server is not available via the REST API, it needs to be manually set so that all timestamps are displayed with the correct timezone. By default the timezone will be set to the local timezone of the PowerShell environment.")][PSObject]$Timezone,
+        [parameter(Mandatory=$False,
+                   Position=6,
+                   HelpMessage="Timeout value for HTTP connections. Defaults to 600 seconds.")][Int]$Timeout
+    )
+
+    if (!$Credential) {
+        $Credential = Get-OciCredential -Name $Name | Select-Object -ExpandProperty Credential
+        if (!$Credential) {
+            throw "No Credentials supplied and $Name not in list of known OCI Servers"
+        }
+    }
+ 
+    # Issue with jBoss see http://alihamdar.com/2010/06/19/expect-100-continue/
+    [System.Net.ServicePointManager]::Expect100Continue = $false
+ 
+    $EncodedAuthorization = [System.Text.Encoding]::UTF8.GetBytes($Credential.UserName + ':' + $Credential.GetNetworkCredential().Password)
+    $EncodedPassword = [System.Convert]::ToBase64String($EncodedAuthorization)
+    $Headers = @{"Authorization"="Basic $($EncodedPassword)"}
+ 
+    # check if untrusted SSL certificates should be ignored
+    if ($Insecure) {
+        [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+    }
+
+    # check if proxy is used
+    $ProxyRegistry = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+    $ProxySettings = Get-ItemProperty -Path $ProxyRegistry
+    if ($ProxySettings.ProxyEnable) {
+        Write-Warning "Proxy Server $($ProxySettings.ProxyServer) configured in Internet Explorer may be used to connect to the OCI server!"
+    }
+    if ($ProxySettings.AutoConfigURL) {
+        Write-Warning "Proxy Server defined in automatic proxy configuration script $($ProxySettings.AutoConfigURL) configured in Internet Explorer may be used to connect to the OCI server!"
+    }
+ 
+    if ($HTTPS -or !$HTTP) {
+        Try {
+            $BaseURI = "https://$Name"
+            $Response = Invoke-RestMethod -Method Post -Uri "$BaseURI/rest/v1/login" -TimeoutSec $Timeout -Headers $Headers
+            $APIVersion = $Response.apiVersion
+        }
+        Catch {
+            $ResponseBody = ParseExceptionBody $_.Exception.Response
+            if ($_.Exception.Message -match "Unauthorized") {
+                Write-Error "Authorization for $BaseURI/rest/v1/login with user $($Credential.UserName) failed"
+                return
+            }
+            else {
+                if ($HTTPS) {
+                    Write-Error "Login to $BaseURI/rest/v1/login failed via HTTPS protocol, but HTTPS was enforced. Exception $($_.Exception.Message)`n $ResponseBody"
+                    return
+                }
+                else {
+                    Write-Warning "Login to $BaseURI/rest/v1/login failed via HTTPS protocol. Exception message: $($_.Exception.Message)`n $ResponseBody"
+                    $HTTP = $True
+                }
+            }
+        }
+    }
+
+    if ($HTTP) {
+        Try {
+            $BaseURI = "http://$Name"
+            $Response = Invoke-RestMethod -Method Post -Uri "$BaseURI/rest/v1/login" -TimeoutSec $Timeout -Headers $Headers
+            $APIVersion = $Response.apiVersion
+        }
+        Catch {
+            $ResponseBody = ParseExceptionBody $_.Exception.Response
+            if ($_.Exception.Message -match "Unauthorized") {                
+                Write-Error "Authorization for $BaseURI/rest/v1/login with user $($Credential.UserName) failed"
+                return
+            }
+            else {
+                Write-Error "Login to $BaseURI/rest/v1/login failed via HTTP protocol. Exception message: $($_.Exception.Message)`n $ResponseBody"
+                return
+            }
+        }
+    }
+
+    if (!$Timezone) {
+        $Timezone = [timezone]::CurrentTimeZone
+    }
+    
+    if ($Timezone -isnot [Timezone]) {
+        if ([System.TimeZoneInfo]::GetSystemTimeZones().Id -contains $Timezone) {
+            $Timezone = [System.TimeZoneInfo]::GetSystemTimeZones() | ? { $_.Id -contains $Timezone }
+        }
+        else {
+            $Timezone = [timezone]::CurrentTimeZone
+        }
+    }
+
+    if (!$Timeout) {
+        $Timeout = 600
+    }
+ 
+    $Server = New-Object -TypeName psobject
+    $Server | Add-Member -MemberType NoteProperty -Name Name -Value $Name
+    $Server | Add-Member -MemberType NoteProperty -Name BaseURI -Value $BaseURI
+    $Server | Add-Member -MemberType NoteProperty -Name Credential -Value $Credential
+    $Server | Add-Member -MemberType NoteProperty -Name Headers -Value $Headers
+    $Server | Add-Member -MemberType NoteProperty -Name APIVersion -Value $APIVersion
+    $Server | Add-Member -MemberType NoteProperty -Name Timezone -Value $Timezone
+    $Server | Add-Member -MemberType NoteProperty -Name Timeout -Value $Timeout
+ 
+    if (!$Transient) {
+        Set-Variable -Name CurrentOciServer -Value $Server -Scope Global
+    }
+ 
+    return $Server
+}
 
 <#
     .SYNOPSIS
@@ -20149,11 +20149,6 @@ function Global:Get-OciStoragePoolPerformance {
         if (!$Server) {
             throw "Server parameter not specified and no global OCI Server available. Run Connect-OciServer first!"
         }
-    }
-    )
- 
-    Begin {
-        $Result = $null
     }
    
     Process {
