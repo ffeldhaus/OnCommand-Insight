093b696e65f3c7123df46e3a9c17c3e8e15fca01
Florian Feldhaus <florian.feldhaus@gmail.com>
2016-08-30 01:23:02 +0200

    Updated Pester tests for Connect-OciServer and Get-OciAcquisitionUnits

--- src/OnCommand-Insight.Tests.ps1
+++ src/OnCommand-Insight.Tests.ps1
@@ -1,49 +1,56 @@
 ﻿Import-Module "$PSScriptRoot\OnCommand-Insight"
 
-$Globa:OciServerName = 'ff-oc1.muccbc.hq.netapp.com'
-$Global:OciCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "admin",("admin123" | ConvertTo-SecureString -AsPlainText -Force)
+$OciServerName = 'localhost'
+$OciCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "admin",("admin123" | ConvertTo-SecureString -AsPlainText -Force)
 
-$global:OciTestServer = Connect-OciServer -Name $Name -Credential $Credential -Insecure
+### functions for validating OCI objects
+function ValidateAcquisitionUnit {
+    [CmdletBinding()]
+        
+    PARAM (
+    [parameter(Mandatory=$False,
+                Position=0,
+                ValueFromPipeline=$True,
+                HelpMessage="Expand parameter for underlying JSON object (e.g. expand=datasources)")][PSObject]$AcquisitionUnit
+    )
 
-Describe "Credential Management" {
-    BeforeEach {
-        $Credential = $null
-        $OciCredential = $null
-        $HostName = $null
-        $Password = $null
-
-        Remove-OciCredential -Name example.com
+        Process {
+            $AcquisitionUnit.id | Should BeGreaterThan 0
+            $AcquisitionUnit.self | Should Be "/rest/v1/admin/acquisitionUnits/$($AcquisitionUnit.id)"
+            $AcquisitionUnit.name | Should Be "local"
+            $AcquisitionUnit.ip | Should Match "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"
+            $AcquisitionUnit.status | Should Be "CONNECTED"
+            $AcquisitionUnit.isActive | Should Be "True"
+            $AcquisitionUnit.leaseContract | Should Be 120000
+            $AcquisitionUnit.nextLeaseRenewal | Should BeGreaterThan (Get-Date)
+            $AcquisitionUnit.lastReported | Should BeLessThan (Get-Date)
     }
-    
-    AfterEach {
-        $Credential = $null
-        $OciCredential = $null
-        $HostName = $null
-        $Password = $null
+}
 
-        Remove-OciCredential -Name example.com
-    }
+function ValidateDatasources {
+    [CmdletBinding()]
+        
+    PARAM (
+    [parameter(Mandatory=$False,
+                Position=0,
+                ValueFromPipeline=$True,
+                HelpMessage="Expand parameter for underlying JSON object (e.g. expand=datasources)")][PSObject]$Datasource
+    )
 
-    Context "adding, retrieving and removing of Credentials" {
-        it "successfully adds, retrieves and removes a credential with hostname example.com and valid credential" {
-            $HostName = "example.com"
-            $UserName = "user"
-            $Password = "password"
-            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $UserName,($Password  | ConvertTo-SecureString -AsPlainText -Force)
-
-            Add-OciCredential -Name $HostName -Credential $Credential
-
-            $OciCredential = Get-OciCredential -Name $HostName
-
-            $OciCredential.Credential.UserName | Should Be $UserName
-            $OciCredential.Credential.GetNetworkCredential().Password | Should Be $Password
-
-            Remove-OciCredential -Name $HostName
-
-            $OciCredential = Get-OciCredential -Name $HostName
-
-            $OciCredential | Should Be $null
-        }
+        Process {
+            $Datasource.id | Should BeGreaterThan 0
+            $Datasource.self | Should Be "/rest/v1/admin/datasources/$($Datasource.id)"
+            $Datasource.impactIndex | Should Match "-?[0-9]+"
+            $Datasource.name | Should Match ".+"
+            $Datasource.status | Should Match "[A-Z]+"
+            $Datasource.statusText | Should Match ".+"
+            $Datasource.pollStatus | Should Match "[A-Z]+"
+            $Datasource.vendor | Should Match ".+"
+            $Datasource.foundationIp | Should Match "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"
+            $Datasource.lastSuccessfullyAccquired | Should BeLessThan (Get-Date)
+            if ($Datasource.resumeTime) {
+                $Datasource.resumeTime | Should BeGreaterThan (Get-Date)
+            }
     }
 }
 
@@ -59,26 +66,37 @@ Describe "Connect-OciServer" {
     }
 
     Context "initiates a connection to an OnCommand Insight Server" {
-
-        it "succeeds with parameters Name, Credential and Insecure" {
+        it "succeeds with parameters Name, Credential, Insecure" {
             $OciServer = Connect-OciServer -Name $OciServerName -Credential $OciCredential -Insecure
             $OciServer.Name | Should Be $OciServerName
             $Global:CurrentOciServer | Should Be $OciServer
         }
-    }
-    
-    Context "initiates a connection to an OnCommand Insight Server without setting the global variable `$Global:CurrentOciServer" {
-        it "succeeds with parameters Name, Credential and Transient" {
-            $Server = Connect-OciServer -Name example.com -Credential $Credential -Transient
-            $Server.Name | Should Be $ServerName
+
+        it "succeeds when forcing HTTPS" {
+            $OciServer = Connect-OciServer -Name $OciServerName -Credential $OciCredential -Insecure -HTTPS
+            $OciServer.Name | Should Be $OciServerName
+            $Global:CurrentOciServer | Should Be $OciServer
+        }
+
+        it "succeeds and successfully sets timezone to UTC" {
+            $Timezone = [TimeZoneInfo]::UTC
+            $OciServer = Connect-OciServer -Name $OciServerName -Credential $OciCredential -Insecure -Timezone $Timezone
+            $OciServer.Name | Should Be $OciServerName
+            $OciServer.Timezone | Should Be $Timezone
+            $Global:CurrentOciServer | Should Be $OciServer
+        }
+
+        it "succeeds and returns a transient OCI Server object" {
+            $OciServer = Connect-OciServer -Name $OciServerName -Credential $OciCredential -Transient -Insecure
+            $OciServer.Name | Should Be $OciServerName
             $Global:CurrentOciServer | Should BeNullOrEmpty
         }
     }
 }
 
 Describe "Get-OciAcquisitionUnits" {
+
     BeforeEach {
-        $OciServer = Connect-OciServer -Name $OciServerName -Credential $OciCredential -Insecure
         $Global:CurrentOciServer = $null
         $AcquisitionUnits = $null
     }
@@ -88,14 +106,30 @@ Describe "Get-OciAcquisitionUnits" {
         $Global:CurrentOciServer = $null
     }
 
-    Context "gets Acquisition Units" {
-        $AcquisitionUnits = Get-OciAcquisitionUnits
-        $AcquisitionUnits | Should Not BeNullOrEmpty
-    }
+    Context "retrieves acquisition units" {
+        it "succeeds with no parameters" {
+            $OciServer = Connect-OciServer -Name $OciServerName -Credential $OciCredential -Insecure
 
-    Context "gets Acquisition Units with datasources" {
-        $AcquisitionUnits = Get-OciAcquisitionUnits -datasources
-        $AcquisitionUnits | Should Not BeNullOrEmpty
-        $AcquisitionUnits.datasources | Should Not BeNullOrEmpty
+            $AcquisitionUnits = Get-OciAcquisitionUnits
+            $AcquisitionUnits | Should Not BeNullOrEmpty
+            $AcquisitionUnits | ValidateAcquisitionUnit
+        }
+
+        it "succeeds with transient OCI Server" {
+            $OciServer = Connect-OciServer -Name $OciServerName -Credential $OciCredential -Insecure -Transient
+
+            $AcquisitionUnits = Get-OciAcquisitionUnits -Server $OciServer
+            $AcquisitionUnits | Should Not BeNullOrEmpty
+            $AcquisitionUnits | ValidateAcquisitionUnit
+        }
+
+        it "succeeds with getting datasources" {
+            $OciServer = Connect-OciServer -Name $OciServerName -Credential $OciCredential -Insecure
+
+            $AcquisitionUnits = Get-OciAcquisitionUnits -datasources
+            $AcquisitionUnits | Should Not BeNullOrEmpty
+            $AcquisitionUnits | ValidateAcquisitionUnit
+            $AcquisitionUnits.datasources | ValidateDatasources
+        }
     }
 }
