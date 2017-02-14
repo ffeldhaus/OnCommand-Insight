923a2e353d30e132041e950a689bcd2cf5c21a35
Florian Feldhaus <florian.feldhaus@gmail.com>
2017-02-14 09:04:34 +0100

    Improved Import-OciServerCertificate and Remove-OciServerCertificate
    Added Name parameter for Import-OciServerCertificate and
    Remove-OciServerCertificate to allow connections to the OCI Server
    without providing previously connecting to the OCI Server

--- src/OnCommand-Insight.psm1
+++ src/OnCommand-Insight.psm1
@@ -1275,7 +1275,11 @@ function global:Connect-OciServer {
     Import OCI Server Certificate into Windows Certificate store
     .DESCRIPTION
     Import OCI Server Certificate into Windows Certificate store
-    .PARAMETER server
+    .PARAMETER CertificateStoreOwner
+    Owner of the certificate store where the certificate should be stored.
+    .PARAMETER Name
+    The name of the OCI Server. This value may also be a string representation of an IP address. If not an address, the name must be resolvable to an address.
+    .PARAMETER Server
     OCI Server to connect to
     .EXAMPLE
     Import-OciServerCertificate
@@ -1289,30 +1293,40 @@ function global:Import-OciServerCertificate {
                    HelpMessage="Owner of the certificate store where the certificate should be stored.")][ValidateSet("CurrentUser","LocalMachine")][String]$CertificateStoreOwner="CurrentUser",
         [parameter(Mandatory=$False,
                    Position=1,
+                   HelpMessage="The name of the OCI Server. This value may also be a string representation of an IP address. If not an address, the name must be resolvable to an address.")]$Name,
+        [parameter(Mandatory=$False,
+                   Position=2,
                    HelpMessage="OnCommand Insight Server.")]$Server=$CurrentOciServer
     )
 
     Begin {
         $Result = $null
-        if (!$Server) {
-            throw "Server parameter not specified and no global OCI Server available. Run Connect-OciServer first!"
+        if (!$Server -and !$Name) {
+            throw "Server and Name parameter not specified and no global OCI Server available. Use Name parameter or run Connect-OciServer first!"
+        }
+        if ($Name) {
+            $Uri = "https://$Name"
+        }
+        else {
+            $Uri = $Server.BaseUri
+        }
+        if ($CertificateStoreOwner -eq "LocalMachine" -and ![Security.Principal.WindowsPrincipal]::new([Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
+            throw "Administrator privilige required to store certificate in LocalMachine certificate store"
         }
     }
    
     Process {
-        $Request = [Net.HttpWebRequest]::Create($Server.BaseUri)
+        $Request = [Net.HttpWebRequest]::Create($Uri)
         $Request.Method = "OPTIONS"
 
         try {
             $Response = $Request.GetResponse()
         }
         catch {
-            $ResponseBody = ParseExceptionBody $_.Exception.Response
-            Write-Error "GET to $Uri failed with Exception $($_.Exception.Message) `n $responseBody"
         }
  
         if (!$Request.ServicePoint.Certificate) {
-            Write-Error "No Certificate returned for $($Server.BaseUri)"
+            Write-Error "No Certificate returned for $Uri"
         }
 
         $Certificate = $Request.ServicePoint.Certificate
@@ -1341,7 +1355,11 @@ function global:Import-OciServerCertificate {
     Remove OCI Server Certificate from Windows Certificate store
     .DESCRIPTION
     Remove OCI Server Certificate from Windows Certificate store
-    .PARAMETER server
+    .PARAMETER CertificateStoreOwner
+    Owner of the certificate store where the certificate should be removed from.
+    .PARAMETER Name
+    The name of the OCI Server. This value may also be a string representation of an IP address. If not an address, the name must be resolvable to an address.
+    .PARAMETER Server
     OCI Server to connect to
     .EXAMPLE
     Remove-OciServerCertificate
@@ -1352,33 +1370,43 @@ function global:Remove-OciServerCertificate {
     PARAM (
         [parameter(Mandatory=$False,
                    Position=0,
-                   HelpMessage="Owner of the certificate store where the certificate should be stored.")][ValidateSet("CurrentUser","LocalMachine")][String]$CertificateStoreOwner="CurrentUser",
+                   HelpMessage="Owner of the certificate store where the certificate should be removed from.")][ValidateSet("CurrentUser","LocalMachine")][String]$CertificateStoreOwner="CurrentUser",
         [parameter(Mandatory=$False,
                    Position=1,
+                   HelpMessage="The name of the OCI Server. This value may also be a string representation of an IP address. If not an address, the name must be resolvable to an address.")]$Name,
+        [parameter(Mandatory=$False,
+                   Position=2,
                    HelpMessage="OnCommand Insight Server.")]$Server=$CurrentOciServer
     )
 
     Begin {
         $Result = $null
-        if (!$Server) {
-            throw "Server parameter not specified and no global OCI Server available. Run Connect-OciServer first!"
+        if (!$Server -and !$Name) {
+            throw "Server and Name parameter not specified and no global OCI Server available. Use Name parameter or run Connect-OciServer first!"
+        }
+        if ($Name) {
+            $Uri = "https://$Name"
+        }
+        else {
+            $Uri = $Server.BaseUri
+        }
+        if ($CertificateStoreOwner -eq "LocalMachine" -and ![Security.Principal.WindowsPrincipal]::new([Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
+            throw "Administrator privilige required to remove certificate from LocalMachine certificate store"
         }
     }
    
     Process {
-        $Request = [Net.HttpWebRequest]::Create($Server.BaseUri)
+        $Request = [Net.HttpWebRequest]::Create($Uri)
         $Request.Method = "OPTIONS"
 
         try {
             $Response = $Request.GetResponse()
         }
         catch {
-            $ResponseBody = ParseExceptionBody $_.Exception.Response
-            Write-Error "GET to $Uri failed with Exception $($_.Exception.Message) `n $responseBody"
         }
  
         if (!$Request.ServicePoint.Certificate) {
-            Write-Error "No Certificate returned for $($Server.BaseUri)"
+            Write-Error "No Certificate returned for $Uri"
         }
 
         $Certificate = $Request.ServicePoint.Certificate
