4d483c68b4ad09aa6b0f3c0f3a5cc1bd904ebb49
Florian Feldhaus <florian.feldhaus@gmail.com>
2017-03-03 11:35:18 +0100

    Updated Update-OciLicenses cmdlet

--- src/OnCommand-Insight.psm1
+++ src/OnCommand-Insight.psm1
@@ -3648,19 +3648,13 @@ function Global:Get-OciLicenses {
     }
 }
 
-# Todo: Implement / Test Updating of licenses
 <#
     .SYNOPSIS
     Update license information
     .DESCRIPTION
-    Request body should be list of license keys, example: <br/>
-
-<pre>
-[
-    "TEST1234567890LicenceKey1",
-    "TEST1234567890LicenceKey2"
-]
-</pre>
+    Update license information
+    .PARAMETER Licenses
+    List of OCI Licenses
     .PARAMETER server
     OCI Server to connect to
 #>
@@ -3668,7 +3662,12 @@ function Global:Update-OciLicenses {
     [CmdletBinding()]
  
     PARAM (
-
+        [parameter(Mandatory=$True,
+                   Position=0,
+                   HelpMessage="List of OCI licenses")][String[]]$Licenses,
+        [parameter(Mandatory=$False,
+                   Position=1,
+                   HelpMessage="OnCommand Insight Server.")]$Server=$CurrentOciServer
     )
  
     Begin {
@@ -3676,61 +3675,26 @@ function Global:Update-OciLicenses {
         if (!$Server) {
             throw "Server parameter not specified and no global OCI Server available. Run Connect-OciServer first!"
         }
-
-        $switchparameters=@()
-        foreach ($parameter in $switchparameters) {
-            if ((Get-Variable $parameter).Value) {
-                if ($expand) {
-                    $expand += ",$($parameter -replace 'performancehistory','performance.history' -replace 'hostswitch','host')"
-                }
-                else {
-                    $expand = $($parameter -replace 'performancehistory','performance.history' -replace 'hostswitch','host')
-                }
-            }
-        }
     }
    
     Process {
-        $id = @($id)
-        foreach ($id in $id) {
-            $Uri = $Server.BaseUri + "/rest/v1/admin/license"            
+        $Uri = $Server.BaseUri + "/rest/v1/admin/license"            
  
-            if ($fromTime -or $toTime -or $expand) {
-                $Uri += '?'
-                $Separator = ''
-                if ($fromTime) {
-                    $Uri += "fromTime=$($fromTime | ConvertTo-UnixTimestamp)"
-                    $Separator = '&'
-                }
-                if ($toTime) {
-                    $Uri += "$($Separator)toTime=$($toTime | ConvertTo-UnixTimestamp)"
-                    $Separator = '&'
-                }
-                if ($expand) {
-                    $Uri += "$($Separator)expand=$expand"
-                }
-            }
- 
-            try {
-                if ('PATCH' -match 'PUT|POST') {
-                    Write-Verbose "Body: "
-                    $Result = Invoke-RestMethod -TimeoutSec $Server.Timeout -Method PATCH -Uri $Uri -Headers $Server.Headers -Body "" -ContentType 'application/json'
-                }
-                else {
-                    $Result = Invoke-RestMethod -TimeoutSec $Server.Timeout -Method PATCH -Uri $Uri -Headers $Server.Headers
-                }
-            }
-            catch {
-                $ResponseBody = ParseExceptionBody $_.Exception.Response
-                Write-Error "PATCH to $Uri failed with Exception $($_.Exception.Message) `n $responseBody"
-            }
- 
-            if (([String]$Result).Trim().startsWith('{') -or ([String]$Result).toString().Trim().startsWith('[')) {
-                $Result = ParseJsonString($Result.Trim())
-            }
-           
-            Write-Output $Result
+        try {
+            $Body = $Licenses | ConvertTo-Json -Compress
+            Write-Verbose "Body: $Body"
+            $Result = Invoke-RestMethod -TimeoutSec $Server.Timeout -Method PATCH -Uri $Uri -Headers $Server.Headers -Body $Body -ContentType 'application/json'
         }
+        catch {
+            $ResponseBody = ParseExceptionBody $_.Exception.Response
+            Write-Error "PATCH to $Uri failed with Exception $($_.Exception.Message) `n $responseBody"
+        }
+ 
+        if (([String]$Result).Trim().startsWith('{') -or ([String]$Result).toString().Trim().startsWith('[')) {
+            $Result = ParseJsonString($Result.Trim())
+        }
+           
+        Write-Output $Result
     }
 }
 
