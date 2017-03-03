69b77e03a105a5e64ce0d2bd76f22c437256fb4f
Florian Feldhaus <florian.feldhaus@gmail.com>
2017-03-03 07:54:42 +0100

    Added experimental Cmdlet to delete an OCI query

--- src/OnCommand-Insight.psm1
+++ src/OnCommand-Insight.psm1
@@ -32291,6 +32291,59 @@ function Global:Get-OciQuery {
 
 <#
     .SYNOPSIS
+    Delete query
+    .DESCRIPTION
+    Delete query
+    .PARAMETER id
+    ID of query to delete
+    .PARAMETER server
+    OCI Server to connect to
+#>
+function Global:Delete-OciQuery {
+    [CmdletBinding()]
+ 
+    PARAM (
+        [parameter(Mandatory=$True,
+                    Position=0,
+                    HelpMessage="ID of query to delete",
+                    ValueFromPipeline=$True,
+                    ValueFromPipelineByPropertyName=$True)][Long[]]$id,
+        [parameter(Mandatory=$False,
+                   Position=1,
+                   HelpMessage="OnCommand Insight Server.")]$Server=$CurrentOciServer
+    )
+ 
+    Begin {
+        Write-Warning "This Cmdlet uses an undocumented API call which may change in the future. Thus this Cmdlet is marked as experimental!"
+        $Result = $null
+        if (!$Server) {
+            throw "Server parameter not specified and no global OCI Server available. Run Connect-OciServer first!"
+        }
+    }
+   
+    Process {
+        $Uri = $Server.BaseUri + "/rest/v1/queries/$id"
+ 
+        try {
+            $Result = Invoke-RestMethod -TimeoutSec $Server.Timeout -Method DELETE -Uri $Uri -Headers $Server.Headers
+        }
+        catch {
+            $ResponseBody = ParseExceptionBody $_.Exception.Response
+            Write-Error "DELETE to $Uri failed with Exception $($_.Exception.Message) `n $responseBody"
+        }
+ 
+        if (([String]$Result).Trim().startsWith('{') -or ([String]$Result).toString().Trim().startsWith('[')) {
+            $Result = ParseJsonString($Result.Trim())
+        }
+
+        # TODO: implement parsing
+        $Query = $Result
+        Write-Output $Query
+    }
+}
+
+<#
+    .SYNOPSIS
     Retrieve annotation rules
     .DESCRIPTION
     Retrieve annotation rules
