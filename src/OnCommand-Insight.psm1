c5ceb7f6fcba7949576edde63953a21432ea85c5
Florian Feldhaus <florian.feldhaus@gmail.com>
2017-03-03 09:58:39 +0100

    Updated and tested Test-OciDatasource

--- src/OnCommand-Insight.psm1
+++ src/OnCommand-Insight.psm1
@@ -3386,7 +3386,6 @@ function Global:Resume-OciDatasource {
     }
 }
 
-# TODO: Implement / Test testing of datasources
 <#
     .SYNOPSIS
     Test Data Source
@@ -3416,47 +3415,15 @@ function Global:Test-OciDatasource {
         if (!$Server) {
             throw "Server parameter not specified and no global OCI Server available. Run Connect-OciServer first!"
         }
-
-        $switchparameters=@()
-        foreach ($parameter in $switchparameters) {
-            if ((Get-Variable $parameter).Value) {
-                if ($expand) {
-                    $expand += ",$($parameter -replace 'performancehistory','performance.history' -replace 'hostswitch','host')"
-                }
-                else {
-                    $expand = $($parameter -replace 'performancehistory','performance.history' -replace 'hostswitch','host')
-                }
-            }
-        }
     }
    
     Process {
         $Uri = $Server.BaseUri + "/rest/v1/admin/datasources/$id/test"            
  
-        if ($fromTime -or $toTime -or $expand) {
-            $Uri += '?'
-            $Separator = ''
-            if ($fromTime) {
-                $Uri += "fromTime=$($fromTime | ConvertTo-UnixTimestamp)"
-                $Separator = '&'
-            }
-            if ($toTime) {
-                $Uri += "$($Separator)toTime=$($toTime | ConvertTo-UnixTimestamp)"
-                $Separator = '&'
-            }
-            if ($expand) {
-                $Uri += "$($Separator)expand=$expand"
-            }
-        }
- 
         try {
-            if ('POST' -match 'PUT|POST') {
-                Write-Verbose "Body: "
-                $Result = Invoke-RestMethod -TimeoutSec $Server.Timeout -Method POST -Uri $Uri -Headers $Server.Headers -Body "" -ContentType 'application/json'
-            }
-            else {
-                $Result = Invoke-RestMethod -TimeoutSec $Server.Timeout -Method POST -Uri $Uri -Headers $Server.Headers
-            }
+            $Body = ""
+            Write-Verbose "Body: $Body"
+            $Result = Invoke-RestMethod -TimeoutSec $Server.Timeout -Method POST -Uri $Uri -Headers $Server.Headers -Body $Body -ContentType 'application/json'
         }
         catch {
             $ResponseBody = ParseExceptionBody $_.Exception.Response
